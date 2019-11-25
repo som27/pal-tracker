e6f02152fa31d8858aab2f7dfddbefdd310072f3
som27 <somyaa.aggarwal@gmail.com>
2019-11-25 15:35:29 +0530

    second assignment

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -5,7 +5,13 @@ namespace PalTracker
     [Route("/")]
     public class WelcomeController : ControllerBase
     {
-        [HttpGet]
-        public string SayHello() => "hello";
+         private readonly WelcomeMessage _message;
+         
+         [HttpGet]
+        public string SayHello() => _message.Message;
+        public WelcomeController(WelcomeMessage message)
+        {
+            _message = message;
+        }
     }
 }
